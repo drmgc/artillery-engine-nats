0479da4c73dab20b8f359a956a26d6b56ce7246b
Sergey Naumov <drmgc@yandex.ru>
2022-11-28 11:50:30 +0600

    tests

--- nats-replier.js
+++ nats-replier.js
@@ -3,8 +3,6 @@ const { connect, StringCodec } = require('nats');
 (async () => {
   const conn = await connect({ servers: process.env.NATS_SERVER });
 
-  // console.log(conn);
-
   const sc = new StringCodec();
 
   async function handleTime(sub) {
@@ -31,8 +29,48 @@ const { connect, StringCodec } = require('nats');
     console.log(`subscription ${sub.getSubject()} drained.`);
   }
 
+  async function handlePing(sub) {
+    console.log(`listening for ${sub.getSubject()} requests...`);
+    for await (const m of sub) {
+      if (m.respond(sc.encode("pong"))) {
+        console.info(`[time] handled #${sub.getProcessed()}`);
+      } else {
+        console.log(`[time] #${sub.getProcessed()} ignored - no reply subject`);
+      }
+    }
+    console.log(`subscription ${sub.getSubject()} drained.`);
+  }
+
+  const code = Math.floor(Math.random() * 1000).toString();
+
+  async function handleGetCode(sub) {
+    console.log(`listening for ${sub.getSubject()} requests...`);
+    for await (const m of sub) {
+      if (m.respond(sc.encode(JSON.stringify({ code })))) {
+        console.info(`[time] handled #${sub.getProcessed()}`);
+      } else {
+        console.log(`[time] #${sub.getProcessed()} ignored - no reply subject`);
+      }
+    }
+    console.log(`subscription ${sub.getSubject()} drained.`);
+  }
+
+  async function handleCode(sub) {
+    console.log(`listening for ${sub.getSubject()} requests...`);
+    for await (const m of sub) {
+      if (m.respond(sc.encode("ok"))) {
+        console.info(`[time] handled #${sub.getProcessed()}`);
+      } else {
+        console.log(`[time] #${sub.getProcessed()} ignored - no reply subject`);
+      }
+    }
+    console.log(`subscription ${sub.getSubject()} drained.`);
+  }
+
   await Promise.all([
     handlePing(conn.subscribe('ping')),
     handleTime(conn.subscribe('time')),
+    handleGetCode(conn.subscribe('get-code')),
+    handleCode(conn.subscribe('code-' + code)),
   ]);
 })();
